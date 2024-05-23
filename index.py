f32c26dedbac3cfecd47ff6f959bd449b739e402
LeakyEarth2000 <yashbatish123@icloud.com>
2024-05-23 23:59:36 +1000

    Created a login system w/ JSON
    - I created a JSON login
    - If valid credentials are present, then it will print out to the terminal that it is successfully logged in, otherwise it will print out invalid username or password, and if the same username exists, it will say that it already exists and won't overwrite the password
    
    I have to make a separate website that will say whether I am logged in or not. Or actually, I will make separate clones of these websites, but put up an error box somewhere that shows what type of error they have and whatever error it is - it will redirect them properly

--- index.py
+++ index.py
@@ -1,62 +1,73 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask import Flask, request, render_template, redirect, url_for
 import json
 
 app = Flask(__name__)
 
 correctPasscode = 1234
 correctPassword = "maths>english"
-redirectURL = 'http://127.0.0.1:5500/ManageUsers/users.html'
+
+# Configure path to user data file
+user_data_file = 'users.json'
+
+def load_users():
+  """Loads user data from JSON file (creates file if it doesn't exist)"""
+  try:
+    with open(user_data_file, 'r') as f:
+      return json.load(f)
+  except FileNotFoundError:
+    with open(user_data_file, 'w') as f:
+      json.dump({}, f, indent=4)  # Create an empty JSON file if not found
+    return {}
+
+def save_users(users):
+  """Saves user data to JSON file"""
+  with open(user_data_file, 'w') as f:
+    json.dump(users, f, indent=4)
+
+users = load_users()  # Load existing users on startup
 
 @app.route("/")
 def hello_world():
-    return render_template('Index/index.html')
-
-# User data storage
-users = {}
+  return render_template('Index/index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        # Handle the form submission
-        username = request.form['username']
-        password = request.form['password']
-        if username in users:
-            return jsonify({"error": "Username already exists"}), 400
-        users[username] = password
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+    if username in users:
+      print("Existing username has already been created!")
+      return render_template('ManageUsers/users.html')
+    users[username] = password
+    save_users(users)
+    return render_template('Index/index.html')
+  else:
+    return render_template('Register/registerAuthorise.html')
 
-        # Save users to a JSON file
-        with open('users.json', 'w') as f:
-            json.dump(users, f)
-
-        return jsonify({"success": "User registered successfully"}), 200
+@app.route('/manageusers', methods=['GET', 'POST'])
+def manageusers():
+  if request.method == 'POST':
+    passcode = request.form['passcode']
+    password = request.form['password']
+    if int(passcode) == correctPasscode and password == correctPassword:
+        return render_template('ManageUsers/users.html')
+    elif password == "english>maths":
+        return "<h1> No, it's not!!</h1>"
     else:
-        # Render the registration form
-        return render_template('Register/registerAuthorise.html')
-
-@app.route('/manage_users', methods=['GET', 'POST'])
-def manage_users():
-    if request.method == 'POST':
-        # Handle the form submission
-        passcode = request.form['passcode']
-        password = request.form['password']
-        if int(passcode) == correctPasscode and password == correctPassword:
-            return render_template('ManageUsers/users.html')
-        elif password == "english>maths":
-            return "<h1> No, it's not!!</h1>"
-        else:
-            return '<h1>Incorrect!</h1>'
-    # Render the users management page
-    return render_template('ManageUsers/users.html')
-
-
+        return '<h1>Incorrect!</h1>'
+  return render_template('Register/registerAuthorise.html')
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
-    if username not in users or users[username] != password:
-        return jsonify({"error": "Invalid username or password"}), 400
-    return jsonify({"success": "User logged in successfully"}), 200
+  username = request.form['username']
+  password = request.form['password']
+  if username not in users or users[username] != password:
+    print("Invalid username or password")
+    return render_template('Index/index.html') # maybe a pop up error, or a modified version of index.html that will say an error? i think its easier to modify the website so that it will show an error instead of a pop up??
+  else:
+    print("Logged in")
+    return render_template('Index/index.html') # must change to a different website
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6969)
+  app.run(host='0.0.0.0', port=6969)
