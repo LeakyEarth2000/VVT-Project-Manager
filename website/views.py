8fc26c3f37d8821080d9b934f653836e8a3cbc39
LeakyEarth2000 <yashbatish123@icloud.com>
2024-05-26 02:13:19 +1000

    Big changes created
    - Made separate python files to keep track of stuff easier and it's significantly easier to manage
    - Instead of .JSON for logins, I used a MYSQL .DB, and it has sha256 encryption, meaning I cannot see the passwords, which is brilliant for security
    - I also started to design a home page prior to the entire project manager, just so it's cleaner

---
+++ website/views.py
@@ -0,0 +1,12 @@
+from flask import Flask, flash, request, render_template, redirect, url_for, Blueprint
+from flask_login import login_required, current_user
+
+import json
+import csv
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+# @login_required
+def home():
+    return render_template('index.html')
